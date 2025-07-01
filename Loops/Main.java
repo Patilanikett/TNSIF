30f47da857c1f799c680dcf046a615cd3cfb269d
Aniket Patil <patilaniket7757@gmail.com>
2025-07-01 11:35:42 +0530

    Add Main class with ForloopExample method for iteration demonstration

---
+++ Loops/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void ForloopExample() {
+        int no_of_iteration = 10;
+
+        for (int i = 1; i <= no_of_iteration; i++) {
+            System.out.println("Iteration " + i);
+        }
+    }
+
+    public static void main(String[] args) {
+        ForloopExample();
+    }
+}
