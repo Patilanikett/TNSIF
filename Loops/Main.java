dd155e229061244ab101639847bb3d085381510a
Aniket Patil <patilaniket7757@gmail.com>
2025-07-01 11:40:59 +0530

    Add WhileExample method to demonstrate while loop functionality

--- Loops/Main.java
+++ Loops/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void ForloopExample() {
+        System.out.println("\nfor loop example");
         int no_of_iteration = 10;
 
         for (int i = 1; i <= no_of_iteration; i++) {
@@ -7,7 +8,20 @@ public class Main {
         }
     }
 
+    public static void WhileExample()
+    {
+        System.out.println("\nwhile loop example");
+        int no_of_iteration=15;
+        int count=1;
+        while (count<no_of_iteration) {
+            System.out.println("iteration"+ count);
+            count++; 
+            
+        }
+    }
+
     public static void main(String[] args) {
         ForloopExample();
+        WhileExample();
     }
 }
