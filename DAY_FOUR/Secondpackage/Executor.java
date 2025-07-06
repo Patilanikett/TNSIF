59ed61c0dc19f7c23eb5ef390e5b2d8c67ecea1f
Aniket Patil <patilaniket7757@gmail.com>
2025-07-06 19:43:14 +0530

    Add Executor class with main method demonstrating Base class functionality

---
+++ DAY_FOUR/Secondpackage/Executor.java
@@ -0,0 +1,12 @@
+import com.tnsif.dayfour.firstpackage.Base;
+
+public class Executor {
+
+	public static void main(String[] args) {Base b1 = new Base();
+        b1.methodPublic();
+		b1.varPublic=34567;
+		b1.methodPublic();
+
+	}
+
+}
