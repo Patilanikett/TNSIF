b8216f2a839f48b0c125c19369ecf571f7f6ad40
Aniket Patil <patilaniket7757@gmail.com>
2025-07-13 21:35:37 +0530

    Implement banking system with Account, Bank interface, and exception handling; add multithreading support for deposits and withdrawals

---
+++ DAY_THIRTEEN/DepositLimitException.java
@@ -0,0 +1,11 @@
+public class DepositLimitException extends Exception {
+
+	public DepositLimitException()
+	{
+		super("Daily Limit of deposit is exceed.....");
+	}
+	public DepositLimitException(String message )
+	{
+		super (message);
+	}
+}
